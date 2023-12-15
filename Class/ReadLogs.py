6928e9584a60a75835cf9e0a0c3b0b088cb3711b
Ricardo Martins <ormartins02@gmail.com>
2023-12-14 23:13:18 -0300

    feat: base window and file Menu

--- Class/ReadLogs.py
+++ Class/ReadLogs.py
@@ -7,20 +7,16 @@ class ReadLogs:
 
         with open(fileLog, "r", encoding="utf-16-le") as arquivo:
             linhas = arquivo.readlines()
-
-        for linha in linhas:
-            if campaign in linha:
-                # linha.strip()
-                line = linha.split("\t")
-                
-                for word in line:
-                    newWord = word.replace("\n", "")
-                
-                # exibitionList.append(lineList)
-                print(line)
-
-        # print(exibitionList)
         
+        lineList = [x.strip('\n') for x in linhas]
+        
+        for linha in lineList:
+            if campaign in linha:
+                line = linha.split("\t")
+                exibitionList.append(line)
+
+        print(exibitionList)
+
         # with open("exemplo.csv", "w", newline="") as arquivo_csv:
         #     escritor_csv = csv.writer(arquivo_csv)
 
--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
 from Class.ReadLogs import ReadLogs
+from Layout.MainWindow import window
 
 campaign = "Live"
 
 pathLogFiles = "logs/"
 
-fileLog = pathLogFiles + '2023-07-31.log'
+fileLog = pathLogFiles + "2023-07-31.log"
 
 teste = ReadLogs()
 
-teste.campaignExtractor(fileLog, campaign)
+# teste.campaignExtractor(fileLog, campaign)
 
+window.mainloop()
