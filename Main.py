a2fec93ebbf7ac916a5c3375d4456f451fc31f03
Ricardo Martins <ormartins02@gmail.com>
2024-01-31 09:58:41 -0300

    file generate and imges for the grafic interface

--- Main.py
+++ Main.py
@@ -1,72 +1,35 @@
+import customtkinter
 import os
-import openpyxl
-from datetime import datetime
-
-campaign1 = "PI 70485"
-
-pathLogFiles = "src/logs/"
-
-actual_date = datetime.now()
-
-date_format = "%H:%M:%S - %d/%m/%Y"
-
-hourFormat = "%H:%M:%S"
-
-organized_date = actual_date.strftime(date_format)
+from PIL import Image
 
 
-def campaignExtractor(campaign):
-    exibitionList = []
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-    for file_name in os.listdir(pathLogFiles):
-        fileLog = os.path.join(pathLogFiles, file_name)
+        self.title("Opec Automation")
+        self.geometry("1280x720")
 
-        if fileLog.endswith(".log"):
-            with open(fileLog, "r", encoding="utf-16-le") as file:
-                linhas = file.readlines()
+        self.grid_rowconfigure(0, weigth=1)
+        self.grid_columnconfigure(1, weigth=1)
 
-            lineList = [word.strip("\n") for word in linhas]
-
-            for line in lineList:
-                if campaign in line:
-                    newLine = line.split("\t")
-                    dateHourObject = datetime.strptime(newLine[0], "%d-%m-%Y %H:%M:%S")
-                    if dateHourObject.hour == 7:
-                        newLine.append("TJM1ED")
-                    elif dateHourObject.hour == 12 or dateHourObject.hour == 12:
-                        newLine.append("TJM2ED")
-                    elif dateHourObject.hour == 19 or dateHourObject.hour == 20:
-                        newLine.append("TJM3ED")
-                    else:
-                        newLine.append("ROTATIVO")
-                    exibitionList.append(newLine)
-
-    final_file = "Report_Template.xlsx"
-
-    cellsPosition = [1, 3, 5, 10, 11]
-
-    def insertDataIntoCells(sheet, data, start_line):
-        sheet.cell(row=8, column=11, value=organized_date)
-
-        for i, line in enumerate(data):
-            for j, value in enumerate(line):
-                for k, position in enumerate(cellsPosition):
-                    if j == k:
-                        sheet.cell(row=start_line + i, column=position, value=value)
-
-    workbook = openpyxl.load_workbook(final_file)
-
-    sheet = workbook.worksheets[0]
-
-    start_line = 20
-
-    insertDataIntoCells(sheet, exibitionList, start_line)
-
-    workbook.save(f"Report_Template.xlsx")
-
-    print(f"Relatório {final_file} atualizado com sucesso.")
+        image_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "src/img"
+        )
+        self.small_logo = customtkinter.CTkImage(
+            Image.open(os.path.join(image_path, "small_logo.png")),
+            size=(26, 26),
+        )
+        self.banner_home = customtkinter.CTkImage(
+            Image.open(os.path.join(image_path, "banner_home.png")),
+            size=(500, 150),
+        )
+        self.add_user_image = customtkinter.CTkImage(
+            Image.open(os.path.join(image_path, "add_user_image.png")),
+            size=(20, 20),
+        )
 
 
-campaignExtractor(campaign1)
-
-# window.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
